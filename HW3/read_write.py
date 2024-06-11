4ee8bb6b87f63c63d68b615153718094d06e8c0f
Mobin <mb.davaji@yahoo.com>
2024-06-11 17:55:08 +0330

    .

--- HW3/read_write.py
+++ HW3/read_write.py
@@ -10,18 +10,7 @@ def sed(input_file_name, output_file_name,pattern_string,replacemetn_string):
 
             with open(output_file_name, 'w', newline='') as fout:
 
-                # for item in csv_reader:
 
-                #     row = ''
-                #     print(item)
-                #     for x in range(len(item)):
-
-                #         row += item
-                #         row += ','
-                #         if pattern_string in row:
-                #             row.replace(pattern_string,replacemetn_string)
-
-                #     fout.writelines(row[:-1])
                 if pattern_string in csv_reader:
                     csv_reader = csv_reader.replace(pattern_string,replacemetn_string)
 
